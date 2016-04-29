33f54097915022ac6283e90f270c2fe4d4b2cd12
Ramon Klass <tier@schokokeks.org>
2016-04-29 18:31:17 +0200

    reload image every 5 minutes or when clicking on it

--- cress/main/urls.py
+++ cress/main/urls.py
@@ -4,11 +4,12 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
-from .views import HomePageView
+from .views import HomePageView, latest_photo
 
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^latest_photo$', latest_photo, name='photojson'),
     url(r'^about/$', TemplateView.as_view(template_name='main/about.html'), name='about'),
     url(r'^legal/$', TemplateView.as_view(template_name='main/legal.html'), name='legal'),
     url(r'^login/$', auth_views.login, {'template_name': 'main/login.html'}, name='login'),
--- cress/main/views.py
+++ cress/main/views.py
@@ -1,4 +1,7 @@
+import json
+
 from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 
 from box.models import Photo, Sensor
 
@@ -14,3 +17,12 @@ class HomePageView(TemplateView):
         if Sensor.objects.order_by('-created').first():
             context['sensors'] = Sensor.objects.order_by('-created')[:4]
         return context
+
+
+def latest_photo(request):
+    photo = Photo.objects.order_by('-created').first()
+    if photo:
+        return HttpResponse(json.dumps({
+            'url': photo.image.url
+        }))
+    return HttpResponse("{}")
