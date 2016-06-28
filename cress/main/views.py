a130d861f38bbe7b561436897544f8ad6eefcd28
Andreas Madsack <andreas@madflex.de>
2016-06-28 22:56:00 +0200

    exclude purged images on frontpage

--- cress/main/views.py
+++ cress/main/views.py
@@ -24,15 +24,16 @@ class HomePageView(TemplateView):
             cycle_list = Cycle.objects.exclude(active__isnull=True).filter(box=Box.objects.get(pk=1)).order_by('start_date')
         context["cycle_list"] = cycle_list
         context["cycle_set"] = cycle_active.first().pk
-        photos = Photo.objects.filter(cycle=cycle_active.first()).order_by('-created')
+        photos = Photo.objects.exclude(purged=True).filter(cycle=cycle_active.first()).order_by('-created')
         sensors = Sensor.objects.filter(cycle=cycle_active.first()).order_by('-created')
         if photos.first():
-            context['image'] = photos.first()
-            days = abs((photos.last().created - photos.first().created).days)
+            photo = photos.first()
+            context['image'] = photo
+            days = abs((photos.last().created - photo.created).days)
             context['older_images'] = []
             for day in range(1, days + 1):
-                time_threshold = photos.first().created - datetime.timedelta(days=day)
-                context['older_images'].append(photos.filter(created__lt=time_threshold).first())
+                time_threshold = photo.created - datetime.timedelta(days=day)
+                context['older_images'].append(photos.exclude(purged=True).filter(created__lt=time_threshold).first())
         if sensors.first():
             if cycle_active.filter(active=True):
                 context['sensor_list'] = sensors[:7]
@@ -40,7 +41,7 @@ class HomePageView(TemplateView):
             # only one value per hour.
             chart_data = chart_data.filter(created__minute=chart_data.first().created.minute).order_by('created')
             context['chart_data'] = chart_data
-        photos = Photo.objects.filter(cycle__box=3).order_by('-created')
+        photos = Photo.objects.exclude(purged=True).filter(cycle__box=3).order_by('-created')
         if photos:
             context['outside_image'] = photos.first()
         return context
