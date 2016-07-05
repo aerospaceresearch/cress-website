0c9c128f351264595abc3b4e7eb61e9219e238de
Andreas Madsack <andreas@madflex.de>
2016-07-05 20:17:05 +0200

    add sessionauth

--- cress/cress/settings/base.py
+++ cress/cress/settings/base.py
@@ -164,5 +164,6 @@ SITE_ID = 1
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
