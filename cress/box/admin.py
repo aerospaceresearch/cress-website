e57717f42ef9d9a7463e9e7d012fc13177247d75
Andreas Madsack <andreas@madflex.de>
2016-07-05 20:17:58 +0200

    action to purge every second image

--- cress/box/admin.py
+++ cress/box/admin.py
@@ -14,11 +14,20 @@ class CycleAdmin(admin.ModelAdmin):
     list_display = ('box', 'name', 'plant', 'start_date', 'active')
 
 
+def delete_file(obj):
+    if obj.image and os.path.isfile(obj.image.path):
+        os.remove(obj.image.path)
+        obj.purged = True
+        obj.save()
+        return 1
+    return 0
+
+
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('owner', 'image', 'cycle', 'created', 'not_purged')
     list_filter = ('cycle', 'purged')
-    actions = ('purge_images', )
+    actions = ('purge_images', 'purge_images_half')
 
     def not_purged(self, obj):
         return not obj.purged
@@ -28,18 +37,25 @@ class PhotoAdmin(admin.ModelAdmin):
         yesterday = datetime.date.today() - datetime.timedelta(days=1)
         count = 0
         for element in queryset.exclude(created__gte=yesterday):
-            if element.image:
-                if os.path.isfile(element.image.path):
-                    os.remove(element.image.path)
-                    element.purged = True
-                    element.save()
-                    count += 1
+            count += delete_file(element)
         if count:
             msg = "%s images were successfully purged." % count
         else:
             msg = "No images were purged."
         self.message_user(request, msg)
     purge_images.short_description = "Purge selected images if older than yesterday"
+
+    def purge_images_half(self, request, queryset):
+        yesterday = datetime.date.today() - datetime.timedelta(days=1)
+        count = 0
+        for element in queryset.exclude(created__gte=yesterday).exclude(created__minute__in=[i for i in range(0, 60, 10)]):
+            count += delete_file(element)
+        if count:
+            msg = "%s images were successfully purged." % count
+        else:
+            msg = "No images were purged."
+        self.message_user(request, msg)
+    purge_images_half.short_description = "Purge selected images if older than yesterday and not divisable on 10."
 
 
 @admin.register(Sensor)
