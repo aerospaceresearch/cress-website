1b231827627c17aa4f260ff299502a83d88f8163
Andreas Madsack <andreas@madflex.de>
2016-07-15 15:48:47 +0200

    changes in config for automation of letsencrypt
    using https://github.com/hlandau/acme

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ docker build --tag=cress-prod .
 docker rm -f cress
 docker run -d --volumes-from cress-data --link cress-db:db -v `pwd`/cress/cress/settings/production.py:/opt/code/cress/cress/settings/production.py --restart=always --name cress cress-prod
 docker rm -f cress-nginx
-docker run --name cress-nginx --net="host" --volumes-from cress-data -p 80:80 -v `pwd`/nginx.conf:/etc/nginx/nginx.conf --restart=always -d nginx
+docker run --name cress-nginx --net="host" --volumes-from cress-data -p 80:80 -v `pwd`/nginx.conf:/etc/nginx/nginx.conf -v /var/lib/acme:/var/lib/acme --restart=always -d nginx
 
 echo "Cleaning up old docker images..."
 docker rmi $(docker images | grep "<none>" | awk '{print($3)}')
