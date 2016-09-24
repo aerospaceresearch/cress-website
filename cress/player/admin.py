8c5e1954d33042ac9e90a3c372aefca03a6d684c
Andreas Madsack <andreas@madflex.de>
2016-09-24 16:10:57 +0200

    remove player models

--- cress/player/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Vote
-
-
-@admin.register(Vote)
-class VoteAdmin(admin.ModelAdmin):
-    list_display = ('user', 'cycle', 'action_type', 'decision')
-    list_filter = ('action_type', 'cycle')
