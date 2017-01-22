a96f4c6b4ac84fbe4e41d29d80a96a9a31b53c7e
Andreas Madsack <andreas@madflex.de>
2017-01-22 22:10:20 +0100

    add django dev toolbar for development

--- cress/cress/settings/dev.py
+++ cress/cress/settings/dev.py
@@ -3,6 +3,40 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE_CLASSES = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+] + MIDDLEWARE_CLASSES
+
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
+def show_toolbar(request):
+    return True
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': 'cress.settings.dev.show_toolbar',
+    # Rest of config
+}
+
+STATIC_URL = '/static/'
+
 AUTH_PASSWORD_VALIDATORS = []
 
 # SECURITY WARNING: keep the secret key used in production secret!
--- cress/cress/urls.py
+++ cress/cress/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -6,3 +7,10 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('box.urls')),
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
