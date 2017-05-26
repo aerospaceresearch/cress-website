6ad511187f87c75c994911761b20022036420772
Andreas Madsack <andreas@madflex.de>
2017-05-26 18:24:54 +0200

    write box/cycle name into filename

--- generate_zips.sh
+++ generate_zips.sh
@@ -4,9 +4,13 @@
 BASE="cress/media"
 EXPORT_FOLDER=`pwd`/export
 
-for i in $(ls -1 export/*.list); do
-    echo "$i"
+for i in $(ls -1 export/*30*.list); do
     cycle=`echo $i | cut -d'_' -f2`
-    echo $cycle
-    (cd $BASE/photo/c$cycle; zip ${EXPORT_FOLDER}/cycle_${cycle}_images.zip *.jpeg)
+    # get box and box_cycle from csv file
+    box=`tail $i -n +2 | tail -1 | cut -d';' -f2`
+    box_cycle=`tail $i -n +2 | tail -1 | cut -d';' -f3`
+    zipfile="${EXPORT_FOLDER}/cycle_pk${cycle}_box${box}_${box_cycle}_images.zip"
+    if [ ! -f $zipfile ]; then
+        (cd $BASE/photo/c$cycle; zip $zipfile *.jpeg)
+    fi
 done
