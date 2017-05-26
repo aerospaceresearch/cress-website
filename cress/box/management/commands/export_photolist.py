f77c40d84886230f5914a158d4b7d5627fdd0ea9
Andreas Madsack <andreas@madflex.de>
2017-05-26 18:24:42 +0200

    write box/cycle to csv file

--- cress/box/management/commands/export_photolist.py
+++ cress/box/management/commands/export_photolist.py
@@ -17,12 +17,14 @@ class Command(BaseCommand):
         if cycle:
             cycle_list = [cycle]
         else:
-            cycle_list = range(3, Cycle.objects.exclude(pk=9).exclude(active=True).order_by("-pk").first().pk + 1)
+            cycle_list = list(Cycle.objects.exclude(pk=9).exclude(active=True).order_by("pk").values_list('pk', flat=True))
         for cycle in cycle_list:
             fn = "/opt/code/export/cycle_{}_photo.list".format(cycle)
             print(fn)
             with open(fn, "w") as fp:
-                fp.write('"id";"filename"\n')
+                fp.write('"id";"box";"box_cycle";"filename"\n')
                 for element in qs.filter(cycle_id=cycle).order_by('created'):
                     if element.photo:
-                        fp.write('{};"{}"\n'.format(element.pk, element.photo.name))
+                        box = '{:d}'.format(int(element.cycle.box.description.split(' ')[-1]))
+                        box_cycle = element.cycle.name.split(' ')[-1]
+                        fp.write('{};{};{};"{}"\n'.format(element.pk, box, box_cycle, element.photo.name))
