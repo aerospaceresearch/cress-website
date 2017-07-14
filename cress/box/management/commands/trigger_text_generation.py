81186e482f44fa3b95bca3acec2998e6777e7c6a
Andreas Madsack <andreas@madflex.de>
2017-07-14 19:29:28 +0200

    add more data for text_generation

--- cress/box/management/commands/trigger_text_generation.py
+++ cress/box/management/commands/trigger_text_generation.py
@@ -8,13 +8,28 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         from ax.models import AxText
-        from box.models import Cycle
+        from box.models import Cycle, Plant, Sensor
 
-        # FIXME: fetch some data
+        # cycle 9 is the mounted camera and no real box
+        active_cycles = Cycle.objects.exclude(pk=9).filter(active=True)
+        active_ids = active_cycles.values_list('id', flat=True)
+        temp_outside = Sensor.objects.filter(cycle=9, value_type='temperature').order_by('-created').first()
+        humidity_outside = Sensor.objects.filter(cycle=9, value_type='humidity').order_by('-created').first()
+
         data = {
-            # cycle 9 is the mounted camera and no real box
-            'active_boxes_count': Cycle.objects.exclude(pk=9).filter(active=True).count(),
+            'active_boxes_count': active_cycles.count(),
+            'plants': {},
+            'outside_temperature': temp_outside.value,
+            'outside_temperature_unit': temp_outside.unit,
+            'outside_humidity': humidity_outside.value,
+            'outside_humidity_unit': humidity_outside.unit,
         }
+        for plant in Plant.objects.all():
+            c = plant.cycle_set.filter(pk__in=active_ids)
+            if c:
+                data['plants'][plant.name_en] = {
+                    'count': c.count()
+                }
 
         obj = AxText.objects.create()
         data['uid'] = obj.pk
